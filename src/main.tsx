992b44d0307bd8486f67112fede391cecb6f184e
klaercio <klaerciosilva@gmail.com>
2022-05-10 21:59:53 -0300

    👨🏽‍🚀Botão de chat finalizado

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+import APP  from './App';
+
+import './main.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <APP />
+  </React.StrictMode>
+)

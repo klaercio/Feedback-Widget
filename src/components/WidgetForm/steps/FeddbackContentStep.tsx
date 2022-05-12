4ed9c0a5516a8120ab451a783565acfeea811631
klaercio <klaerciosilva@gmail.com>
2022-05-12 19:46:58 -0300

    🤳🏽Função de tirar uma screenshot da tela adicionada

--- src/components/WidgetForm/steps/FeddbackContentStep.tsx
+++ src/components/WidgetForm/steps/FeddbackContentStep.tsx
@@ -1,6 +1,7 @@
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
 import { FeedbackType, feedbackTypes} from ".."
 import CloseButton from "../../CloseButton"
+import ScreenshotButton from "./ScreenshotButton";
 
 interface FeedbackContentStepProps {
     feedbackType : FeedbackType;
@@ -29,7 +30,8 @@ export default function FeedbackContentStep({feedbackType, onFeedbackRestartRequ
                 placeholder="Conte com detalhes o que está acontecendo"
             />
 
-            <footer className="flex m-2 mt-2">
+            <footer className="flex gap-2 mt-2">
+                <ScreenshotButton/>
                 <button
                     type="submit"
                     className="p-2 bg-brand-500 rounded-[4px] border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors">Enviar Feedback
